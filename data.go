6a68473b8672d17fa1aa1a78c836dc33d92a6e5b
juicypy <juicypiesounds@gmail.com>
2018-05-05 13:03:12 +0300

    added script escaper for map

--- data.go
+++ data.go
@@ -16,24 +16,36 @@ var escaper = strings.NewReplacer(
 // ScriptEscaper verify each string structure field and
 // escapes special characters like "<" to become "&lt;".
 // It escapes only five such characters: <, >, &, ' and ".
-func ScriptEscaper(structptr interface{}) {
-	escape(structptr, escaper)
+func StructScriptEscaper(structptr interface{}) {
+	structEscape(structptr, escaper)
 }
 
 // CharEscaper works the same as ScriptEscaper, but escapes only character you need
-func CharEscaper(structptr interface{}, oldstr string, newstr string){
+func StructCharEscaper(structptr interface{}, oldstr string, newstr string){
 	newEscaper := strings.NewReplacer(oldstr, newstr)
-	escape(structptr, newEscaper)
+	structEscape(structptr, newEscaper)
 
 }
 
-func escape(structptr interface{}, replacer *strings.Replacer){
+func MapScriptEscaper(mapPtr *map[string]interface{}){
+	mapEscape(mapPtr, escaper)
+}
+
+func structEscape(structptr interface{}, replacer *strings.Replacer){
 	t := reflect.ValueOf(structptr).Elem()
 	fieldsLen := t.NumField()
 
 	for i := 0; i < fieldsLen; i++ {
 		if t.Field(i).Type().String() == "string" {
 			t.Field(i).SetString(replacer.Replace(t.Field(i).String()))
+		}
+	}
+}
+
+func mapEscape(mapPtr *map[string]interface{}, replacer *strings.Replacer){
+	for k, v := range *mapPtr{
+		if strValue, ok := v.(string); ok{
+			(*mapPtr)[k] = interface{}(replacer.Replace(strValue))
 		}
 	}
 }
